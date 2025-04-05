a6de086a17eb017393411988e5653fec32dd94f7
Vanessa-21if <21vanessaaa@gmail.com>
2025-04-05 17:37:02 -0500

    Update app(m).py

--- app/app(m).py
+++ app/app(m).py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, HTTPException, Request
 import uvicorn
 from app.controlador.MedicationCrud import (
-    GetMedicationRequestById,
-    WriteMedicationRequest,
-    GetMedicationRequestByIdentifier,
+    GetmedicationRequestById,
+    WritemedicationRequest,
+    GetmedicationRequestByIdentifier,
 )
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -19,28 +19,28 @@ app.add_middleware(
 
 @app.get("/medicationRequest/{request_id}", response_model=dict)
 async def get_request_by_id(request_id: str):
-    status, request = GetMedicationRequestById(request_id)
+    status, request = GetmedicationRequestById(request_id)
     if status == 'success':
         return request
     elif status == 'notFound':
-        raise HTTPException(status_code=404, detail="MedicationRequest not found")
+        raise HTTPException(status_code=404, detail="medicationRequest not found")
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
 @app.get("/medicationRequest", response_model=dict)
 async def get_request_by_identifier(system: str, value:str):
-    status, request =GetMedicationRequestByIdentifier(system, value)
+    status, request =GetmedicationRequestByIdentifier(system, value)
     if status == 'success':
         return request
     elif status == 'notFound':
-        raise HTTPException(status_code=204, detail="MedicationRequest not found")
+        raise HTTPException(status_code=204, detail="medicationRequest not found")
     else: 
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
 @app.post("/medicationRequest", response_model=dict)
 async def add_request(request: Request):
     new_request_dict = dict(await request.json())
-    status, request_id = WriteMedicationRequest(new_request_dict)
+    status, request_id = WritemedicationRequest(new_request_dict)
     if status == 'success':
         return {"_id": request_id}
     else:
